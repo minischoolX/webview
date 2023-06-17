f9a9e4d55aef27c2d06a5e74cfcf4e3138da6558
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-17 21:50:18 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.JsResult;
-import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
