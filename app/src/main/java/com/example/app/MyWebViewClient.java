ddca53a92c4813a05e71f6d2b99c05fdc2f9f5da
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-18 15:51:17 +0530

    Update MyWebViewClient.java

--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -9,17 +9,133 @@ class MyWebViewClient extends WebViewClient {
 
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-        String hostname;
-
-        // YOUR HOSTNAME
-        hostname = "m.youtube.com";
-
-        Uri uri = Uri.parse(url);
-        if (url.startsWith("file:") || uri.getHost() != null && uri.getHost().endsWith(hostname)) {
-            return false;
-        }
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        view.getContext().startActivity(intent);
+        view.loadUrl(url);
         return true;
     }
+
+        @Override
+    public void onPageFinished(WebView view, String url) {
+        super.onPageFinished(view, url);
+
+        String javascriptCode = "const canvas = document.createElement('canvas');\n" +
+        "canvas.width = 16;\n" +
+        "canvas.height = 9;\n" +
+        "\n" +
+        "const context = canvas.getContext('2d');\n" +
+        "context.fillStyle = 'black';\n" +
+        "context.fillRect(0, 0, canvas.width, canvas.height);\n" +
+        "\n" +
+        "const imageDataURL = canvas.toDataURL('image/jpeg');\n" +
+        "\n" +
+        "function addDynamicStyles() {\n" +
+        "  const style = document.createElement('style');\n" +
+        "  style.innerHTML = `\n" +
+        "    /* CSS styles for the sticky floating block */\n" +
+        "    #dynamicContent {\n" +
+        "      position: fixed;\n" +
+        "      bottom: 0;\n" +
+        "      left: 0;\n" +
+        "      width: 100%;\n" +
+        "      height: 40%;\n" +
+        "      background-color: #f1f1f1;\n" +
+        "      padding: 20px;\n" +
+        "      box-sizing: border-box;\n" +
+        "      overflow-y: auto;\n" +
+        "    }\n" +
+        "  `;\n" +
+        "  document.head.appendChild(style);\n" +
+        "}\n" +
+        "\n" +
+        "function updateDynamicContent() {\n" +
+        "  const currentURL = window.location.href;\n" +
+        "  const host = window.location.host;\n" +
+        "  const hasValidHost =\n" +
+        "    host.includes('m.youtube.com') ||\n" +
+        "    host.includes('www.youtube.com') ||\n" +
+        "    host.includes('youtube.com') ||\n" +
+        "    host.includes('w3schools.com');\n" +
+        "  const hasValidQuery = currentURL.includes('watch');\n" +
+        "  const videoElement = document.querySelector('video');\n" +
+        "  if (!videoElement.getAttribute('poster')) {\n" +
+        "    videoElement.setAttribute('poster', imageDataURL);\n" +
+        "  }\n" +
+        "  if (hasValidHost && hasValidQuery) {\n" +
+        "    const videoId = getVideoIdFromUrl(`${currentURL}`);\n" +
+        "    const deviceWidth = Math.min(\n" +
+        "      window.innerWidth ||\n" +
+        "        document.documentElement.clientWidth ||\n" +
+        "        document.body.clientWidth,\n" +
+        "      window.screen.width ||\n" +
+        "        window.screen.availWidth ||\n" +
+        "        document.documentElement.offsetWidth\n" +
+        "    );\n" +
+        "    const deviceHeight = Math.min(\n" +
+        "      window.innerHeight ||\n" +
+        "        document.documentElement.clientHeight ||\n" +
+        "        document.body.clientHeight,\n" +
+        "      window.screen.height ||\n" +
+        "        window.screen.availHeight ||\n" +
+        "        document.documentElement.offsetHeight\n" +
+        "    );\n" +
+        "    const baseDimension = Math.max(deviceWidth, deviceHeight);\n" +
+        "    let posterUrl = '';\n" +
+        "    if (baseDimension < 320) {\n" +
+        "      posterUrl = `https://i.ytimg.com/vi_webp/${videoId}/default.webp`;\n" +
+        "    } else if (baseDimension < 480) {\n" +
+        "      posterUrl = `https://i.ytimg.com/vi_webp/${videoId}/mqdefault.webp`;\n" +
+        "    } else if (baseDimension < 640) {\n" +
+        "      posterUrl = `https://i.ytimg.com/vi_webp/${videoId}/hqdefault.webp`;\n" +
+        "    } else if (baseDimension < 1280) {\n" +
+        "      posterUrl = `https://i.ytimg.com/vi_webp/${videoId}/sddefault.webp`;\n" +
+        "    } else {\n" +
+        "      posterUrl = `https://i.ytimg.com/vi_webp/${videoId}/maxresdefault.webp`;\n" +
+        "    }\n" +
+        "    videoElement.setAttribute('poster', posterUrl);\n" +
+        "  }\n" +
+        "  const attributesSet = videoElement && videoElement.attributes.poster && videoElement.attributes.poster.value\n" +
+        "    ? 'Yes'\n" +
+        "    : 'No';\n" +
+        "  const videoElementHTML = videoElement ? escapeHtml(videoElement.outerHTML) : '';\n" +
+        "  const dynamicContentDiv = document.createElement('div');\n" +
+        "  dynamicContentDiv.id = 'dynamicContent';\n" +
+        "  dynamicContentDiv.innerHTML = `\n" +
+        "    <p>Host: ${host}</p>\n" +
+        "    <p>URL: ${currentURL}</p>\n" +
+        "    <p>Conditions satisfied: ${hasValidHost && hasValidQuery}</p>\n" +
+        "    <ul>\n" +
+        "      <li>ValidHost: ${hasValidHost}</li>\n" +
+        "      <li>ValidQuery: ${hasValidQuery}</li>\n" +
+        "    </ul>\n" +
+        "    <p>Video Element:</p>\n" +
+        "    <pre><code>${videoElementHTML}</code></pre>\n" +
+        "  `;\n" +
+        "  document.body.appendChild(dynamicContentDiv);\n" +
+        "}\n" +
+        "\n" +
+        "function escapeHtml(text) {\n" +
+        "  const map = {\n" +
+        "    '&': '&amp;',\n" +
+        "    '<': '&lt;',\n" +
+        "    '>': '&gt;',\n" +
+        "    '\"': '&quot;',\n" +
+        "    \"'\": '&#039;'\n" +
+        "  };\n" +
+        "  return text.replace(/[&<>\"']/g, m => map[m]);\n" +
+        "}\n" +
+        "\n" +
+        "function getVideoIdFromUrl(url) {\n" +
+        "  const regex = /[?&]v=([^&#]*)/;\n" +
+        "  const match = url.match(regex);\n" +
+        "  return match && match[1] ? match[1] : '';\n" +
+        "}\n" +
+        "\n" +
+        "updateDynamicContent();";
+
+//webView.evaluateJavascript(javascriptCode, null);
+
+                view.loadUrl("javascript:" javascriptCode);
+
+        
+    }
+
 }
