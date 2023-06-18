1f1e346ce2ea9544769df0b74daa297e0e3867c5
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-18 22:17:21 +0530

    Update MyWebViewClient.java

--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -17,7 +17,7 @@ class MyWebViewClient extends WebViewClient {
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
 
-String javascriptCode = "function e(e){var n=document.createElement(\"div\");n.textContent=e,document.body.appendChild(n)}window.addEventListener(\"hashchange\",(function(){document.body.innerHTML=\"\",setTimeout((function(){document.querySelector(\"ytm-app\")?e(\"<ytm-app> found.\"):e(\"No <ytm-app> found.\")}),5e3)}));";
+String javascriptCode = "var e=document.getElementsByTagName(\"body\")[0],t=document.createElement(\"p\");new MutationObserver((function(n){var a=function(){if(document.getElementsByTagName(\"video\").length>0)return\"Video tag found directly on the page.\";for(var e=document.getElementsByTagName(\"iframe\"),t=0;t<e.length;t++){var n=e[t];if((n.contentDocument||n.contentWindow.document).getElementsByTagName(\"video\").length>0)return\"Video tag found within an iframe with src: \"+n.src}for(var a=document.getElementsByClassName(\"dynamic-element\"),o=0;o<a.length;o++)if(a[o].getElementsByTagName(\"video\").length>0)return\"Video tag found within a dynamically added element.\";return\"Video tag not found.\"}();t.textContent=a,setTimeout((function(){e.innerHTML=\"\",e.appendChild(t)}),7e3)})).observe(e,{childList:!0,subtree:!0});";
 
 //webView.evaluateJavascript(javascriptCode, null);
 
