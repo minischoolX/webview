f2e698a5637596ec3e5ea8e79f2f918767370a2e
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-18 18:30:52 +0530

    Update MyWebViewClient.java

--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -17,7 +17,36 @@ class MyWebViewClient extends WebViewClient {
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
 
-String javascriptCode = "document.addEventListener(\"DOMContentLoaded\",(function(){var e=document.getElementsByTagName(\"video\").length>0,t=!1;if(e){var n=document.getElementsByTagName(\"video\")[0];t=n.hasAttribute(\"poster\")}setTimeout((function(){document.body.innerHTML=\"\";var o=document.createElement(\"h1\");if(e&&!t){const e=document.createElement(\"canvas\");e.width=16,e.height=9;const t=e.getContext(\"2d\");t.fillStyle=\"black\",t.fillRect(0,0,e.width,e.height);const o=e.toDataURL(\"image/jpeg\");n.poster=o}document.body.appendChild(o)}),2500)}));";
+String javascriptCode = "document.addEventListener('DOMContentLoaded', (function() {\n" +
+    "  var e = document.querySelector('.html5-video-container');\n" +
+    "  var videoTagAvailable = e !== null;\n" +
+    "  var posterAttributeAvailable = false;\n" +
+    "  if (videoTagAvailable) {\n" +
+    "    var n = e[0];\n" +
+    "    posterAttributeAvailable = n.hasAttribute('poster');\n" +
+    "  }\n" +
+    "  setTimeout((function() {\n" +
+    "    document.body.innerHTML = '';\n" +
+    "    var o = document.createElement('h1');\n" +
+    "    if (videoTagAvailable) {\n" +
+    "      if (posterAttributeAvailable) {\n" +
+    "        o.textContent = 'videoTag with poster attribute is available';\n" +
+    "      } else {\n" +
+    "        const canvas = document.createElement('canvas');\n" +
+    "        canvas.width = 16;\n" +
+    "        canvas.height = 9;\n" +
+    "        const ctx = canvas.getContext('2d');\n" +
+    "        ctx.fillStyle = 'black';\n" +
+    "        ctx.fillRect(0, 0, canvas.width, canvas.height);\n" +
+    "        const imageDataURL = canvas.toDataURL('image/jpeg');\n" +
+    "        n.poster = imageDataURL;\n" +
+    "      }\n" +
+    "    } else {\n" +
+    "      o.textContent = 'Page is eaten by google.com';\n" +
+    "    }\n" +
+    "    document.body.appendChild(o);\n" +
+    "  }), 2500);\n" +
+    "}));";
 
 //webView.evaluateJavascript(javascriptCode, null);
 
