eab0710ada10e16878a4d6ddcd5aaae73b0eaa50
minischoolX <104305605+minischoolX@users.noreply.github.com>
2023-06-18 17:59:55 +0530

    Update MyWebViewClient.java

--- app/src/main/java/com/example/app/MyWebViewClient.java
+++ app/src/main/java/com/example/app/MyWebViewClient.java
@@ -17,7 +17,7 @@ class MyWebViewClient extends WebViewClient {
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
 
-String javascriptCode = "document.addEventListener(\"DOMContentLoaded\",(function(){var e=document.getElementsByTagName(\"video\").length>0;setTimeout((function(){document.body.innerHTML=\"\";var t=document.createElement(\"h1\");t.textContent=e?\"videoTag is available\":\"Page is eaten by google.com\",document.body.appendChild(t)}),2500)}));";
+String javascriptCode = "document.addEventListener(\"DOMContentLoaded\",(function(){var e=document.getElementsByTagName(\"video\").length>0,t=!1;if(e){var n=document.getElementsByTagName(\"video\")[0];t=n.hasAttribute(\"poster\")}setTimeout((function(){document.body.innerHTML=\"\";var o=document.createElement(\"h1\");if(e&&!t){const e=document.createElement(\"canvas\");e.width=16,e.height=9;const t=e.getContext(\"2d\");t.fillStyle=\"black\",t.fillRect(0,0,e.width,e.height);const o=e.toDataURL(\"image/jpeg\");n.poster=o}document.body.appendChild(o)}),2500)}));";
 
 //webView.evaluateJavascript(javascriptCode, null);
 
